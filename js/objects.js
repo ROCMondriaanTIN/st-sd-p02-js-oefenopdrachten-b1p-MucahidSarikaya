508890b952cb87756df16535695d729ff6fe36ee
MucahidSarikaya <SARM01@rocmondriaan.nl>
2024-11-19 11:46:42 +0100

    Uitleg objecten, properties en ophalen van data uit objecten

---
+++ js/objects.js
@@ -0,0 +1,19 @@
+const arrayUsers = [
+    userOne = {
+        name: 'Hans',
+        age: 15
+    },
+    userTwo = {
+        name: 'Piet',
+        age: 18
+    },
+    userThree = {
+        name: 'Ali',
+        age: 20
+    },
+        
+];
+
+for(let i = 0; i < arrayUsers.length; i++){
+    console.log(arrayUsers[i].name);
+}
